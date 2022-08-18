b35a915b8805b1a2de821dbb8d342b6fa5e83f71
tang <sky3139!@qq.com>
2022-08-18 09:27:27 +0800

    fix para

--- run.sh
+++ run.sh
@@ -2,5 +2,6 @@
 # --img 640 640 --cfg cfg/training/yolov7.yaml --weights '' --name yolov7 --hyp data/hyp.scratch.p6.yaml 
 
 python3 train.py --workers 1 --device 0 --batch-size 1 --data data/coco.yaml \
---img 640 640 --cfg cfg/training/yolov7.yaml --weights '' --name yolov7 --hyp data/hyp.scratch.p5.yaml
+--img 640 640 --cfg cfg/training/yolov7-tiny.yaml --weights '' --name yolov7-tiny --hyp data/hyp.scratch.p5.yaml
 
+python3 detect.py --weights yolov7.pt --conf 0.25 --img-size 640 --source inference/images/horses.jpg
