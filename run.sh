c82e78df2f6720a46f265068004e40f9d7ea447c
tang <sky3139!@qq.com>
2022-08-18 08:00:38 +0800

    fix config

--- run.sh
+++ run.sh
@@ -1,2 +1,6 @@
-python3 train.py --workers 8 --device 0 --batch-size 2 --data data/vis.yaml \
---img 640 640 --cfg cfg/training/yolov7.yaml --weights '' --name yolov7 --hyp data/hyp.scratch.p6.yaml 
+# python3 train.py --workers 8 --device 0 --batch-size 2 --data data/vis.yaml \
+# --img 640 640 --cfg cfg/training/yolov7.yaml --weights '' --name yolov7 --hyp data/hyp.scratch.p6.yaml 
+
+python3 train.py --workers 1 --device 0 --batch-size 1 --data data/coco.yaml \
+--img 640 640 --cfg cfg/training/yolov7.yaml --weights '' --name yolov7 --hyp data/hyp.scratch.p5.yaml
+
